33cf46f6e16d2ecffdbfd3b403975684801749a0
TiKhon-it <tikhon.k.2008@gmail.com>
2024-07-17 19:05:40 +0400

    Add files via upload

---
+++ Sait.py
@@ -0,0 +1,7 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QLabel, QLineEdit, QTextEdit, QApplication
+from playFair import BibaBoba 
+
+class Upravlenie(QWidget):
+    def __init__(self):
+        super().__init__()
